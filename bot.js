6c05e010ac46a64affd8fb3e8f2545b94cb86011
oJayce <54153379+oJayce@users.noreply.github.com>
2019-08-19 15:02:36 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`I am Cute ! `,"http://twitch.tv/Dream")
+client.user.setGame(`Hypixel... ! `,"http://twitch.tv/Dream")
 client.user.setStatus("dnd")
 });
 
