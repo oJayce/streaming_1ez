f5338ce19b3e01e72dbb415ce1e4aef67406c425
oJayce <54153379+oJayce@users.noreply.github.com>
2019-08-19 15:02:09 +0100

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+const client = new Discord.Client();
+var prefix = "+";
+client.on('ready', () => {
+   console.log(`----------------`);
+      console.log(`Cyhper Bot Script By : DREAM`);
+        console.log(`----------------`);
+      console.log(`ON ${client.guilds.size} Servers '     Script By : DREAM ' `);
+    console.log(`----------------`);
+  console.log(`Logged in as ${client.user.tag}!`);
+client.user.setGame(`I am Cute ! `,"http://twitch.tv/Dream")
+client.user.setStatus("dnd")
+});
+
+client.login(process.env.BOT_TOKEN);// لا تغير فيها شيء
